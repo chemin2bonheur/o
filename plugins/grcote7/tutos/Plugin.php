c9fd7868e20e10d7aa44ccf368d5824a5f5f6053
COTE <GrCOTE7@GMail.com>
2018-05-14 14:26:35 +0200

    Create plugin Tutos

---
+++ plugins/grcote7/tutos/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Grcote7\Tutos;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
