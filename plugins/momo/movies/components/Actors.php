fc91c0c0dd800389b2ee0504eb4086123abd23c0
Thioubado <seckndanane16@gmail.com>
2018-06-18 01:51:05 +0100

    momo up version: tuto 17

--- plugins/momo/movies/components/Actors.php
+++ plugins/momo/movies/components/Actors.php
@@ -15,13 +15,65 @@ class Actors extends ComponentBase
         ];
     }
 
+
+    public function defineProperties() {
+
+        return [
+            'results' => [
+                'title' => 'number of actors',
+                'description' => 'how many actors do you want to display',
+                'default' => 0,
+                'validationPattern' => '^[0-9]+$',
+                'validationMessage' => 'Only numbers allowed'
+            ],
+
+            'sortOrder' => [
+                'title' => 'sort Actors',
+                'description' => 'sort those actors',
+                'type' => 'dropdown',
+                'default' => 'name asc'
+
+            ]
+       ];
+    }
+
+    public function getSortOrderOptions() {
+
+        return[
+            'name asc' => 'name ascending',
+            'name desc' => 'name descending'
+        ];
+    }
+
     public function onRun(){
         $this->actors = $this->loadActors();
     }
 
+
+
     protected function loadActors(){
-        return Actor::all();
+
+        $query = Actor::all();
+
+            if($this->property('sortOrder') == 'name asc'){
+                $query = $query ->sortBy('name');
+            }
+
+            if($this->property('sortOrder') == 'name desc'){
+                $query = $query ->sortByDesc('name');
+            }
+
+
+            if( $this->property('results') > 0 ){
+
+                $query = $query -> take($this->property('results'));
+            }
+
+        return $query;
     }
+
+
+
 
     public $actors;
 
