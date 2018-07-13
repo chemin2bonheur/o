9357ff7c6677ac99c43fb7885b0c5fbc4deeadb4
COTE <GrCOTE7@GMail.com>
2018-07-13 18:19:13 +0200

    Change database_name

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver'    => 'mysql',
             'host'      => 'localhost',
             'port'      => 3306,
-            'database'  => 'o',
+            'database'  => 'o_momo',
             'username'  => 'root',
             'password'  => '',
             'charset'   => 'utf8mb4',
