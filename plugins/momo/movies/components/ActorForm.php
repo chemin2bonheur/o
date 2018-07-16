4715894fe177279fa1c9b61c5b0dea0743b3415b
Thioubado <seckndanane16@gmail.com>
2018-07-16 02:26:19 +0100

    up momo:tuto 20 frontend Form corrigé

--- plugins/momo/movies/components/ActorForm.php
+++ plugins/momo/movies/components/ActorForm.php
@@ -8,6 +8,9 @@ use Validator;
 
 use Redirect;
 
+use momo\Movies\Models\Actor;
+
+use Flash;
 
 Class ActorForm extends ComponentBase{
 
@@ -22,10 +25,19 @@ Class ActorForm extends ComponentBase{
     }
 
 
+    public function onRun(){
+//        $this->actors = $this->loadActors();
+    }
+
+
     public function onSave(){
 
-        
-
+        $actor = new Actor();
+        $actor -> name = input::get('name');
+        $actor -> lastname = input::get('lastname');
+        $actor -> save();
+        Flash::success("Actor Added!");
+        return Redirect::back();
 
     }
 
